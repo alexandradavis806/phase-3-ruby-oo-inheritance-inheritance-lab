0dea89f106d6de5ab3b61292cef09e8b3baa2384
Alexandra Davis <alex.davis806@gmail.com>
2022-04-27 18:52:44 -0700

    commit tree 62568d691f7a16e80c2a3ccceaad5ab4498f6189

---
+++ lib/user.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class User
+
+    attr_accessor :first_name, :last_name
+
+    # def initialize(first_name, last_name)
+    #     @first_name = first_name
+    #     @last_name = last_name
+    # end
+
+
+end
+
+# user = User.new('tracy', 'morgan')
+
+# binding.pry
